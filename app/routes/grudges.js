0b02ca0bb08c10b3ce6e32f893906c3b8964b54e
Adam Bernard <ab255@me.com>
2017-01-10 15:24:48 -0700

    fix render of all grudges

--- app/routes/grudges.js
+++ app/routes/grudges.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    grudges: this.get('store').findAll('grudge'),
+    return this.get('store').findAll('grudge');
   },
 });
