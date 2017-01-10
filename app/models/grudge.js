77b248c061b893dfb9e4a93f4141d96af4b049d2
Adam Bernard <ab255@me.com>
2017-01-10 10:28:06 -0700

    add template for new grudge

--- app/models/grudge.js
+++ app/models/grudge.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr('date'),
+  date: DS.attr('date'),
   name: DS.attr('string'),
   wrongs: DS.attr('string'),
   forgiven: DS.attr('boolean', { defaultValue: false })
